200f107ef7327ab355df1844ea7b91b0ec164427
Riyanti Maulya <riyantimaulya17@gmail.com>
2021-10-19 14:40:31 +0700

    update form

--- resources/views/admin/kelas/index.blade.php
+++ resources/views/admin/kelas/index.blade.php
@@ -1,13 +1,21 @@
 <x-app-layout>
 
-  <div class="row">
-    <div class="col-12">
-      <div class="card mb-4">
-        <div class="card-header pb-0">
-          <h6>Data Kelas</h6>
-        </div>
-        <div class="card-body px-0 pt-0 pb-2">
-          <div class="table-responsive p-0">
+
+        <div class="col-md-12 mb-lg-0 mb-4">
+              <div class="card mt-4">
+                <div class="card-header pb-0 p-3">
+                  <div class="row">
+                    <div class="col-6 d-flex align-items-center">
+                      <h6 class="mb-0">Data Kelas</h6>
+                    </div>
+                    <div class="col-6 text-end">
+                      <a class="btn bg-gradient-dark mb-0" href="javascript:;"><i class="fas fa-plus"></i>&nbsp;&nbsp;Tambah Kelas</a>
+                    </div>
+                  </div>
+                </div>
+                <div class="card-body p-3">
+                  <div class="row">
+                  <div class="table-responsive p-0">
             <table id="myTable" class="table align-items-center mb-0">
 
               <thead>
@@ -41,10 +49,11 @@
 
             </table>
           </div>
-        </div>
-      </div>
-    </div>
-  </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+        
   @push('scripts')
   <script>
     $(document).ready(function() {
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('admin.index');
 })->name('dashboard');
-
+Route::get('/form', function () {
+    return view('form');
+})->name('form');
 // Route::get('/konten-vidio', function () {
 //     return view('admin.kontenVidio.index');
 // })->name('konten-vidio');
